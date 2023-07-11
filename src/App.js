d5aa4b23d708a2fb41f1474afe53ba7994fd5aa3
Mike <maochemike@yahoo.fr>
2023-07-12 11:55:48 +1200

    final version - without sounds

--- src/App.js
+++ src/App.js
@@ -5,42 +5,65 @@ import MagicBox from './MagicBox';
 function App() {
   const [show, setshow] = useState(false)
   const [results, setresults] = useState([])
+  const [canPlayBox, setcanPlayBox] = useState(false)
 
-  useEffect(()=>{
-    if(results.length > 0) handleShow()
-    console.log(results);
+  useEffect(() => {
+    const checkFields = () => {
+      if(results.length > 0) {
+        // some return true if it detects one text that is empty
+        const response = results.some((result) => result.text.length === 0);
+        if(response) {
+          setcanPlayBox(false)
+        }
+        else {
+          setcanPlayBox(true)
+          handleShow()
+        }
+      }
+    }
+    checkFields()
   },[results])
 
   const generateResults = () => {
     const form = document.getElementById('form')
-    const inputs = form.querySelectorAll('input');
-    inputs.forEach((input) => {
-      if(input.value.length > 0) {
-
-      } 
-      else {
-        // required fields are highlighted in red
-        input.classList.add('require')
+    const textInputs = form.querySelectorAll('input[type="text"]')
+    const checkboxInputs = form.querySelectorAll('input[type="checkbox"]')
+    // associate results
+    textInputs.forEach((textInput) => {
+      const correspondingCheckbox = Array.from(checkboxInputs).find((checkbox) => checkbox.name === textInput.name);
+      const isChecked = correspondingCheckbox ? correspondingCheckbox.checked : false;
+      const existingValueIndex = results.findIndex((result) => result.key === textInput.name);
+      if (existingValueIndex !== -1) {
+        const updatedResults = [...results];
+        updatedResults[existingValueIndex].text = textInput.value;
+        updatedResults[existingValueIndex].checked = isChecked;
+        setresults(updatedResults);
+      } else {
+        setresults((prevResults) => [...prevResults, { key: textInput.name, text: textInput.value, checked: isChecked }]);
       }
+
+      // show required fields
+      if(textInput.value.length > 0)  {
+        textInput.classList.remove('require')
+      } else {
+        textInput.classList.add('require')
+      }
+        
     })
   }
 
   const handleShow = () => {
     setshow(!show)
   }
-
-  const clearResults = () => {
-    setresults([])
-  }
-
+  
   return (
     <div className="App">
       <div className='Header'>
         <h1>The Magic Box</h1>
-        <h2>Do not know what to do? Touch it to find an activity</h2>
+        <h2>Do not know what to do? Touch this cube to see what event will happen</h2>
       </div>
       <div className='Content'>
-        <MagicBox />
+        <MagicBox canPlayBox={canPlayBox} setcanPlayBox={setcanPlayBox} results={results} setresults={setresults} show={show} setshow={setshow} />
       </div>
       {
         !show && (
@@ -48,24 +71,23 @@ function App() {
             <h1>Write Five Activities</h1>
             {
               Array.from({length: 5}).map((_,index) => {
-              return <input key={index} name={`activity${index}`} type='text' />
+                const uniqueKey = `activity${index}`;
+                const inputId = `input-${uniqueKey}`;
+                return (
+                  <div className='field' key={uniqueKey}>
+                    <label>
+                      <input id={inputId} name={uniqueKey} type='text' placeholder='Typing...' />
+                      <span>Is Positive?</span>
+                      <input type='checkbox' name={uniqueKey} className="checkbox" />
+                    </label>
+                  </div>
+                )
               })
             }
             <button onClick={generateResults} className='btn-shuffle'>generate</button>
           </div>
         )
       }
-      { show && (
-        <div className='Preview'>
-          <h1>Preview</h1>
-          {
-            results && results.map((result, index) => {
-              return <p key={result+index}>{result}</p>
-            })
-          }
-          <button onClick={clearResults} className='btn-clear'>clear</button>
-        </div>
-      )}
     </div>
   );
 }
--- src/MagicBox.js
+++ src/MagicBox.js
@@ -1,11 +1,129 @@
-import React from 'react';
-function MagicBox() {
-  const playMagicBox = () => {
-    console.log('test');
+import React, { useState, useEffect } from 'react';
+function MagicBox({ canPlayBox, setcanPlayBox, results, setresults, show, setshow }) {
+  const [showLabel, setshowLabel] = useState(false)
+  const [play, setplay] = useState(false)
+  const [start, setstart] = useState(false)
+  var positive_emojis = ['🍒','😁','🙂','😊','🤩','💰','👑','💎','💵','🎁','🎉','🪄','🦄','🍀']
+  var negative_emojis = ['🥹','😢','😭','💀','💩','🙁','😕','😓','😣','🧹','⚡','🔥','☔','🥦','🦈','🐍','🦇']
+  const [result, setresult] = useState('')
+  const [data, setdata] = useState([])
+  const [isSpinning, setisSpinning] = useState(true)
+
+  useEffect(() => {
+    if(!canPlayBox) setstart(false)
+    if(play) {
+      const spans = document.querySelectorAll('.first-spin span');
+      function stopSpinning() {
+          spans.forEach((span) => {
+            span.style.animationPlayState = 'paused';
+          });
+      }
+
+      setTimeout(() => {
+        stopSpinning();
+        setisSpinning(false)
+      }, randomNumber(2000,5000));
+    }
+  },[canPlayBox, data, result])
+
+  function randomNumber(min, max) {
+    const minMultiple = Math.ceil(min / 250); // Calculate the minimum multiple of 250 within the range
+    const maxMultiple = Math.floor(max / 250); // Calculate the maximum multiple of 250 within the range
+    const randomMultiple = Math.floor(Math.random() * (maxMultiple - minMultiple + 1)) + minMultiple; // Generate a random multiple within the range
+    return randomMultiple * 250; // Multiply the random multiple by 250 to get the final random number
   }
+
+  
+
+  const startMagicBox = () => {
+    // cannot play
+    if(!canPlayBox) {
+      setshowLabel(true)
+    }
+    else {
+      setshowLabel(false)
+      setstart(true)
+    }
+  }
+
+  const playMagicBox = () => {
+    setplay(true)
+    results.forEach((result) => {
+      const keyExists = data.some((item) => item.key === result.key);
+      let emoji = randomEmoji(result.checked)
+      if (!keyExists) {
+        setdata((prevData) => [
+          ...prevData,
+          { key: result.key, name: result.text, emoji }
+        ]);
+      }
+    })
+
+  }
+
+  const randomEmoji = (value) => {
+    let emoji = null
+    if(value === true) {
+      emoji = positive_emojis[Math.floor(Math.random() * positive_emojis.length)]
+      positive_emojis = positive_emojis.filter((item) => item !== emoji)
+    }
+    else {
+      emoji = negative_emojis[Math.floor(Math.random() * negative_emojis.length)]
+      negative_emojis = negative_emojis.filter((item) => item !== emoji)
+    }
+    return emoji;
+  }
+
+  const clearResults = () => {
+    setisSpinning(true)
+    setshow(false)
+    setstart(false)
+    setplay(false)
+    setshowLabel(false)
+    setdata([])
+    setresults([])
+    
+  }
+
   return (
     <div>
-      <div onClick={playMagicBox}  className='Cube floatingAnimation'>?</div>
+      {start ? (
+        !play && (
+          <div onClick={playMagicBox} className='Cube'><p className='arrows-animation' >press</p></div>
+        )
+      ) : (
+        <div onClick={startMagicBox} className={`Cube floatingAnimation`}>?</div>
+      )}
+      {
+        showLabel && (
+            <p style={{ color: 'red', fontSize: 24, textAlign: 'center'}}>* You need to fill up the form.</p>
+        )
+      }
+      {
+        play && (
+          <div onClick={playMagicBox} id='magicbox' className='Cube'>
+            <div className='first-spin'>
+              {
+                data.map((element) => {
+                  return <span key={element.name}>{element.emoji}</span>
+                })
+              }
+            </div>
+          </div>
+        )
+      }
+       { 
+       !isSpinning  && show && (
+        <div className='Preview'>
+          <h1>References Table</h1>
+          {
+            data && data.map((result, index) => {
+              return <p key={result+index}>{result.emoji}: {result.name}</p>
+            })
+          }
+          <button onClick={clearResults} className={`btn-clear ${data.length > 0 ? null : 'lock'} `} disabled={data.length > 0 ? false : true}>clear</button>
+        </div>
+      )}
     </div>
   );
 }
